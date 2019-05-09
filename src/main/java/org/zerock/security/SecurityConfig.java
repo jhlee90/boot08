0692cb3bd16723d4fc64118d813afed71f8a40d2
sarah <sarah@DESKTOP-V18M9EL>
2019-05-10 01:54:36 +0900

    home commit

---
+++ src/main/java/org/zerock/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package org.zerock.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import lombok.extern.java.Log;
+
+@Log
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		log.info("security config.................");
+	}
+
+}
