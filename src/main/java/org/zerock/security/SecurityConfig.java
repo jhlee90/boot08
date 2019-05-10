eb2b46814350d19183379839ed02788ecfc8b8c4
peter <jaehaklee@bluedigm.com>
2019-05-10 14:34:45 +0900

    processing study

--- src/main/java/org/zerock/security/SecurityConfig.java
+++ src/main/java/org/zerock/security/SecurityConfig.java
@@ -1,8 +1,11 @@
 package org.zerock.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 import lombok.extern.java.Log;
 
@@ -11,6 +14,27 @@ import lombok.extern.java.Log;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		log.info("security config.................");
+		
+		http.authorizeRequests().antMatchers("/guest/**").permitAll();
+		
+		http.authorizeRequests().antMatchers("/manager/**").hasRole("MANAGER");
+		
+		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
+		
+		http.formLogin().loginPage("/login");
+		
+		http.exceptionHandling().accessDeniedPage("/accessDenied");
+		
+		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
 	}
 
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		log.info("build Auth global........");
+		
+		auth.inMemoryAuthentication()
+		.withUser("manager")
+		.password("{noop}1111")
+		.roles("MANAGER");
+	}
 }
