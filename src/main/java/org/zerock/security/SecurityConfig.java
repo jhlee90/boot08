1eb55dee842309f8fbb510fce9a68aba76810e8a
sarah <sarah@DESKTOP-V18M9EL>
2019-05-15 01:15:36 +0900

    보안모듈 반영중

--- src/main/java/org/zerock/security/SecurityConfig.java
+++ src/main/java/org/zerock/security/SecurityConfig.java
@@ -1,17 +1,29 @@
 package org.zerock.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.extern.java.Log;
 
 @Log
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {@Override
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private ZerockUsersService zerockUsersService;
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		log.info("security config.................");
 		
@@ -26,15 +38,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {@Override
 		http.exceptionHandling().accessDeniedPage("/accessDenied");
 		
 		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
+		
+		http.userDetailsService(zerockUsersService);
 	}
 
-	@Autowired
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		log.info("build Auth global........");
 		
-		auth.inMemoryAuthentication()
-		.withUser("manager")
-		.password("{noop}1111")
-		.roles("MANAGER");
+//		auth.inMemoryAuthentication()
+//		.withUser("manager")
+//		.password("{noop}1111")
+//		.roles("MANAGER");
+		
+//		String query1 = "SELECT uid username, upw password, true enabled FROM tbl_members WHERE uid = ?";
+//		
+//		String query2 = "SELECT member uid, role_name role FROM tbl_member_roles WHERE member = ?";
+//		
+//		auth.jdbcAuthentication()
+//		.dataSource(dataSource)
+//		.usersByUsernameQuery(query1)
+//		.rolePrefix("ROLE_")
+//		.authoritiesByUsernameQuery(query2);
+		auth.userDetailsService(zerockUsersService)
+        	.passwordEncoder(passwordEncoder());
 	}
+	
 }
---
+++ src/main/java/org/zerock/security/ZerockUsersService.java
@@ -0,0 +1,38 @@
+package org.zerock.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.zerock.domain.Member;
+import org.zerock.persistence.MemberRepository;
+
+import lombok.extern.java.Log;
+
+@Service
+@Log
+public class ZerockUsersService implements UserDetailsService {
+
+	@Autowired
+	private MemberRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		User sampleUser = new User(username, "{noop}1111", Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")));
+		Optional<Member> result = repo.findById(username);
+		if (result.isPresent()) {
+			Member m = result.get();
+			log.info("user: " + m.getUid() + " / passwd: " + m.getUpw());
+			ZerockSecurityUser user = new ZerockSecurityUser(m);
+			return user;
+		}
+		
+		return null;
+	}
+
+}
